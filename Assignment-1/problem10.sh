acc3b5da8d4d49985512d5cd8f5beb9623b00235
jiyu han <jiyu-han@uiowa.edu>
2017-09-05 20:55:51 -0500

    it's done, I think?

--- Assignment-1/problem10.sh
+++ Assignment-1/problem10.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 tar xzf zip_directory.tar.gz
-for filename in ./zip_directory/*.*; do
-    if grep -e "*.jpg" $filename; then
-        echo "$filename"
-    elif grep -e "*.txt" $filename; then
-        echo "$filename"
-    fi
+mkdir -p ./zip_directory/JPG/
+for filename in ./zip_directory/*.jpg; do
+    mv $filename ./zip_directory/JPG/
 done
+mkdir -p ./zip_directory/TXT/
+for filename in ./zip_directory/*.txt; do
+    mv $filename ./zip_directory/TXT/
+done
+tar -zcvf zip_directory_zipped.tar.gz zip_directory
+rm -r zip_directory
--- Assignment-1/problem5.sh
+++ Assignment-1/problem5.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+#ls -Sr ./Problem_5_hidden_dir/.* 2>&1 | awk 'NR<4' | xargs stat --printf='%i %b %A %h %U %G %s %.16y %n\n' |
 
-ls -Sr ./Problem_5_hidden_dir/.* 2>&1 | awk 'NR<4' | xargs stat --printf='%i %A %h %U %G %s %.16y %n\n'
-
-#ls -S -r -l -i -N --time-style="+%b%e %Y" ./Problem_5_hidden_dir/.* 2>&1 | awk 'NR<4'
+ls -S -r -lis -N --time-style="+%b%e %Y" ./Problem_5_hidden_dir/.* 2>&1 | awk 'NR<4'
